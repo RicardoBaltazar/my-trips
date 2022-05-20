2b5ddad25fc34172552293818f737c5d3a7f6f95
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2022-05-19 21:42:31 -0300

    utilizando dynamic imports para scripts que só funcionam no browser

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
-import Map from 'components/Map'
+// Escolher se o módulo vai ser carregado no SSR ou no browser
+// Melhorando a performance ou conseguindo executar módulos que são carregados só no browser
+import dynamic from 'next/dynamic'
+
+const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
   return <Map />
