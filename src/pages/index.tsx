0017035c0552991c50a03e4938540974294907f7
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2022-05-20 14:52:11 -0300

    criando página de about

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 // Escolher se o módulo vai ser carregado no SSR ou no browser
 // Melhorando a performance ou conseguindo executar módulos que são carregados só no browser
 import dynamic from 'next/dynamic'
+import { InfoOutline } from '@styled-icons/evaicons-outline/InfoOutline'
+
+import LinkWrapper from 'components/LinkWrapper'
 
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
-  return <Map />
+  return (
+    <>
+      <LinkWrapper href="/about">
+        <InfoOutline size={32} aria-label="About" />
+      </LinkWrapper>
+      <Map />
+    </>
+  )
 }
